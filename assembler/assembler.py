0099c2f94f579a2b1f8ba761728b892b09084591
Azer-ID <azerytid@gmail.com>
2024-12-19 12:37:24 +0700

    Fix bugs

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,5 +1,5 @@
 import sys
-from enum_file import *
+from enum import *
 
 label = {}
 
@@ -26,23 +26,25 @@ def valid_format(string: str) -> bool:
     return False
 
 # Parse a single line of code
-def parse(line: str, line_number: int, code_addr: int) -> list[str]:
+def parse(line: str, code_addr: int) -> list[str]:
     binary = []
 
     # Break down the line of code
-    line = replace_tabs(line.strip().upper()).split()
+    line = replace_tabs(line.strip()).split()
 
     if not line:
         # Skip empty line
         return binary
 
     # Find valid instruction
-    inst = find_inst(line[0])
+    inst = find_inst(line[0].upper())
     if inst == -1:
+        if line[0].startswith(';'):
+            return binary
         if not line[0].endswith(':'):
-            raise ValueError(f"Error at line {line_number}: Invalid instruction.")
+            raise ValueError(f"Invalid instruction.")
         elif line[0][:-1] in label.keys():
-            raise ValueError(f"Error at line {line_number}: The label is already declared.")
+            raise ValueError(f"The label is already declared.")
         else:
             label[line[0][:-1]] = code_addr
         return binary
@@ -64,14 +66,14 @@ def parse(line: str, line_number: int, code_addr: int) -> list[str]:
             elif operand in label.keys():
                 binary.append(f"{label[operand] & 0xFFFFFFFFFFFFFFFF:064b}")
             else:
-                raise ValueError(f"Error at line {line_number}: Invalid register or undeclared label.")
+                raise ValueError(f"Invalid register or undeclared label.")
 
     return binary
 
 # Main function
 def main() -> None:
     if len(sys.argv) < 2:
-        print("Usage: python main.py <input_file>")
+        print("Usage: python assembly.py <input_file>")
         sys.exit(1)
 
     input_file = sys.argv[1]
@@ -82,12 +84,12 @@ def main() -> None:
         with open(input_file, "r") as inf, open(output_file, "w") as outf:
             for line_number, line in enumerate(inf, start=1):
                 try:
-                    binary_line = parse(line, line_number, code_addr)
+                    binary_line = parse(line, code_addr)
                     if binary_line:
                         outf.write(' '.join(binary_line) + '\n')
                         code_addr += 1
                 except ValueError as e:
-                    print(e)
+                    print(f"{output_file}:{line_number}: Error: {e}")
                     sys.exit(1)
     except Exception as e:
         print(f"Error: {e}")
